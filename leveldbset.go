b493aab17be93add78327fd431f3f308cb7e1a8e
Harshad Patil <harshad.patil@goibibo.com>
2015-07-30 00:37:21 +0530

    fixed syntax errors

--- leveldbset.go
+++ leveldbset.go
@@ -1,71 +1,69 @@
 package leveldbset
 
 import (
-	"github.com/syndtr/goleveldb/leveldb",
-	"time",
-	"strconv",
-	"sync/atmoic"
+	"errors"
+	"github.com/syndtr/goleveldb/leveldb"
+	"strconv"
+	"sync/atomic"
+	"time"
 )
 
 const DECIMAL_BASE = 10
 
-type LeveldbSet struct{
-	name string
-	db   *leveldb.DB
-	capacity uint64
+type LeveldbSet struct {
+	name     string
+	db       *leveldb.DB
+	capacity int64
 }
 
 func New(name string, db *leveldb.DB) *LeveldbSet {
 	return &LeveldbSet{name: name, db: db}
 }
 
-func (s *LeveldbSet) Add(element string) error{
+func (s *LeveldbSet) Add(element string) error {
 
-	key := name + "#" + element
-	
+	key := s.name + "#" + element
+
 	now := time.Now().Unix()
 	nowS := strconv.FormatInt(now, DECIMAL_BASE)
-	
-	
-	err := db.Put([]byte(key), []byte(nowS))
+
+	err := s.db.Put([]byte(key), []byte(nowS), nil)
 	if err == nil {
-		atomic.AddUint64(&(s.capacity), 1)
+		atomic.AddInt64(&(s.capacity), 1)
 	}
-	
+
 	return err
 }
 
-func (s *LeveldbSet) Capacity() uint64{
+func (s *LeveldbSet) Capacity() int64 {
 	return s.capacity
 }
 
-func (s *LeveldbSet) Remove(element string) error{
-	key := name + "#" + element
-	
-	
-	val, err := s.db.Get([]byte(key))
+func (s *LeveldbSet) Remove(element string) error {
+	key := s.name + "#" + element
+
+	_, err := s.db.Get([]byte(key), nil)
 	if err == leveldb.ErrNotFound {
-	
+
 		return errors.New("key not found")
-	} elseif err != nil {
-	
-		return errors.New(err)
+	} else if err != nil {
+
+		return err
 	}
-	
-	
-	err = s.db.Delete([]byte(key))
+
+	err = s.db.Delete([]byte(key), nil)
 	if err == nil {
-		atomic.AddUint64(&(s.capacity), -1)
+		atomic.AddInt64(&(s.capacity), -1)
 	}
-	
+
 	return err
 }
 
 func (s *LeveldbSet) IsEmpty() bool {
 
-	if s.capacity == 0{
+	if s.capacity == 0 {
 		return true
 	}
-	
+
 	return false
 }
