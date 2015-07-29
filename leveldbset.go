a0be5d4dd193c59cacf2c2753d11eab5b907b5e0
Harshad Patil <harshad.patil@goibibo.com>
2015-07-30 01:04:45 +0530

    added set pop

--- leveldbset.go
+++ leveldbset.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/syndtr/goleveldb/leveldb"
 	"strconv"
+	"strings"
 	"sync/atomic"
 	"time"
 )
@@ -22,7 +23,7 @@ func New(name string, db *leveldb.DB) *LeveldbSet {
 
 func (s *LeveldbSet) Add(element string) error {
 
-	key := s.name + "#" + element
+	key := s.encodeKey(element)
 
 	now := time.Now().Unix()
 	nowS := strconv.FormatInt(now, DECIMAL_BASE)
@@ -35,12 +36,20 @@ func (s *LeveldbSet) Add(element string) error {
 	return err
 }
 
+func (s *LeveldbSet) encodeKey(key string) string {
+	return s.name + "#" + key
+}
+
+func (s *LeveldbSet) decodeKey(key string) string {
+	return strings.TrimPrefix(key, s.name+"#")
+}
+
 func (s *LeveldbSet) Capacity() int64 {
 	return s.capacity
 }
 
 func (s *LeveldbSet) Remove(element string) error {
-	key := s.name + "#" + element
+	key := s.encodeKey(element)
 
 	_, err := s.db.Get([]byte(key), nil)
 	if err == leveldb.ErrNotFound {
@@ -57,6 +66,18 @@ func (s *LeveldbSet) Remove(element string) error {
 	}
 
 	return err
+}
+
+func (s *LeveldbSet) Pop() string {
+	iter := s.db.NewIterator(nil, nil)
+
+	iter.Next()
+	key := iter.Key()
+	s.db.Delete([]byte(key), nil)
+
+	elem := s.decodeKey(string(key))
+
+	return elem
 }
 
 func (s *LeveldbSet) IsEmpty() bool {
